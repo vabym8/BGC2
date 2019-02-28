e4099df18fecbee6b109d14fcb8b913f5b0a8c51
Aby Mathew <vabym8@gmail.com>
2019-02-28 11:15:24 +0300

    Create text_style.dart

---
+++ text_style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class StyleText{
+  static var textStyle = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 25.0,
+    color: Color(0xFFD0021B),
+  );
+}
